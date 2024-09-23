af22975176a0c4fbe502bcffd1c63c8f3637a342
Farshad Karimi <krfarshad@gmail.com>
2024-09-23 10:34:55 +0330

    chore: save default sidebar behavior in storage

--- client/src/layouts/MUIAppLayout.tsx
+++ client/src/layouts/MUIAppLayout.tsx
@@ -5,23 +5,35 @@ import Drawer from "@mui/material/Drawer";
 import CssBaseline from "@mui/material/CssBaseline";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import { ChildrenProps } from "@/types";
 import { Sidebar } from "./Sidebar";
 import { IconButton } from "@mui/material";
-import { ArrowRight2 } from "iconsax-react";
 import { Header } from "./Header";
-
+import MenuOpenOutlinedIcon from "@mui/icons-material/MenuOpenOutlined";
+import { useLayoutEffect, useState } from "react";
 interface Props extends ChildrenProps {}
 export default function MUIAppLayout(props: Props) {
   const { children } = props;
-  const [open, setOpen] = React.useState(false);
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  useLayoutEffect(() => {
+    if (typeof window !== "undefined") {
+      const savedState = localStorage.getItem("sidebarOpen");
+      if (savedState) {
+        setOpen(JSON.parse(savedState));
+      }
+    }
+  }, []);
 
   const handleDrawerOpen = () => {
-    setOpen(!open);
+    const newState = !open;
+    setOpen(newState);
+    localStorage.setItem("sidebarOpen", JSON.stringify(newState));
   };
   const drawerWidth = open ? 240 : 0;
 
+  console.log("open", open);
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -42,12 +54,12 @@ export default function MUIAppLayout(props: Props) {
             sx={[
               {
                 marginRight: 5,
+                color: "#fff",
                 transition: "all .3s",
               },
-              open && { transform: "rotate(180deg)" },
             ]}
           >
-            <ArrowRight2 size="24" color="#fff" />
+            <MenuOpenOutlinedIcon fontSize="small" color="inherit" />
           </IconButton>
           <Header />
         </Toolbar>
