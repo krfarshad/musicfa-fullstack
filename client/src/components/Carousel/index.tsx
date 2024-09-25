a154cfe2a6094cb7186bf1a70cbc70f7d61061d6
Farshad Karimi <krfarshad@gmail.com>
2024-09-25 16:01:04 +0330

    feat: manage carousel

--- client/src/components/Carousel/index.tsx
+++ client/src/components/Carousel/index.tsx
@@ -1,8 +1,32 @@
-import Carousel from "react-material-ui-carousel";
+"use client";
 import { ChildrenProps } from "@/types";
+import useEmblaCarousel from "embla-carousel-react";
+import { EmblaOptionsType } from "embla-carousel";
+import clsx from "clsx";
 
-interface Props extends ChildrenProps {}
-const CarouselWrapper = (props: Props) => {
-  const { children } = props;
-  return <Carousel>{}</Carousel>;
+
+interface Props extends ChildrenProps {
+  options?: EmblaOptionsType;
+  gap?: `gap-${string}`;
+  className?: string;
+}
+export type { EmblaOptionsType as SliderOptions };
+
+export const Carousel = (props: Props) => {
+  const { children, gap, className } = props;
+
+  const [emblaRef] = useEmblaCarousel();
+  const classes = clsx(
+    "embla__container flex",
+    gap ? gap : "gap-3",
+    className ? className : "",
+  );
+
+  return (
+    <>
+      <div className="embla overflow-hidden" ref={emblaRef}>
+        <div className={classes}>{children}</div>
+      </div>
+    </>
+  );
 };
