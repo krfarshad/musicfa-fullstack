5157efdec80f9e421291493889fe11986b06ecc8
Farshad Karimi <krfarshad@gmail.com>
2024-10-14 11:37:17 +0330

    feat: add artists page

--- client/src/features/artist/components/Pages/Artists/index.tsx
+++ client/src/features/artist/components/Pages/Artists/index.tsx
@@ -1,3 +1,17 @@
+import { Section, SingerCard } from "@/components";
+import { topArtistList } from "@/features/artist/utils/data";
+import { Grid2, Box } from "@mui/material";
+
 export const Artists = () => {
-  return <div>Artists</div>;
+  return (
+    <Section title="Artists" variant="h1">
+      <Grid2 container spacing={2}>
+        {topArtistList.map((artist) => (
+          <Box key={`singer_${artist.id}`}>
+            <SingerCard artist={artist} />
+          </Box>
+        ))}
+      </Grid2>
+    </Section>
+  );
 };
