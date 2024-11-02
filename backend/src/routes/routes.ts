e860191e41ffa6e4204425635fcc706d7a2ebc73
Farshad Karimi <krfarshad@gmail.com>
2024-11-02 18:46:06 +0330

    chore: add 404 route

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -6,10 +6,14 @@ import { playlistsRouter } from "./playlists/playlist-route";
 import { musicsRouter } from "./musics/music-router";
 
 const router: Router = Router();
+router.use(artistsRouter);
 router.use(authRouter);
 router.use(albumsRouter);
-router.use(artistsRouter);
 router.use(playlistsRouter);
 router.use(musicsRouter);
+
+router.use((req, res) => {
+  res.sendStatus(404).json({ message: "Route not found!" });
+});
 
 export default router;
