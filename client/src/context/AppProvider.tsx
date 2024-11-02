14f2e39932e8d4c85356b2183db24eab2a398575
Farshad Karimi <krfarshad@gmail.com>
2024-11-02 18:50:23 +0330

    chore: add toast container in app provider

--- client/src/context/AppProvider.tsx
+++ client/src/context/AppProvider.tsx
@@ -6,6 +6,8 @@ import { memo } from "react";
 import { SessionProvider } from "next-auth/react";
 import MUIProvider from "./MUI/MUIProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Flip, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface Props extends ChildrenProps {}
 
@@ -21,6 +23,21 @@ const AppProvider = (props: Props) => {
           {children}
           <NetworkStatus />
         </MUIProvider>
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          limit={3}
+          pauseOnHover
+          theme="light"
+          transition={Flip}
+          className="md:text-md text-sm"
+        />
       </QueryClientProvider>
     </SessionProvider>
   );
