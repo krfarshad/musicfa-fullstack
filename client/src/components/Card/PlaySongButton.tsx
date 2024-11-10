1be374d7ac90bfe5de97d919f624d857bb172612
Farshad Karimi <krfarshad@gmail.com>
2024-11-10 16:05:08 +0330

    feat: play and pause song  button and sync with slice

--- client/src/components/Card/PlaySongButton.tsx
+++ client/src/components/Card/PlaySongButton.tsx
@@ -1,16 +1,42 @@
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import { IconButton } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
 import PauseIcon from "@mui/icons-material/Pause";
+import { MusicResponse } from "@/utils/models";
+import { useAppDispatch } from "@/hooks";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
+import { playMusicAsync, toggleIsPlaying } from "@/store/slices/musicSlice";
 
 type Props = {
-  status: "play" | "pause";
+  music: MusicResponse;
 };
 export const PlaySongButton = (props: Props) => {
-  const { status } = props;
+  const { music } = props;
+  const dispatch = useAppDispatch();
+  const { isPlaying, currentMusic } = useSelector(
+    (state: RootState) => state.music,
+  );
+
+  const handlePlayMusic = () => {
+    if (currentMusic && currentMusic.id === music.id) {
+      dispatch(toggleIsPlaying(true));
+    } else {
+      dispatch(playMusicAsync(music));
+    }
+  };
+
+  const handlePauseMusic = () => {
+    dispatch(toggleIsPlaying(false));
+  };
+
   return (
     <>
-      <IconButton aria-label="play" color="inherit" size="small">
-        {status == "play" ? <PlayArrowIcon /> : <PauseIcon />}
+      <IconButton aria-label="play" color="inherit" size="large">
+        {currentMusic?.id === music.id && isPlaying ? (
+          <PauseIcon color="secondary" onClick={handlePauseMusic} />
+        ) : (
+          <PlayArrowIcon color="secondary" onClick={handlePlayMusic} />
+        )}
       </IconButton>
     </>
   );
