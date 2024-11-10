2c260c7cb4d1c1ae115404ac59009d0c62893ee7
Farshad Karimi <krfarshad@gmail.com>
2024-11-10 16:00:05 +0330

    refactor: manage layout and context

--- client/src/context/AppProvider.tsx
+++ client/src/context/AppProvider.tsx
@@ -8,6 +8,8 @@ import MUIProvider from "./MUI/MUIProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Flip, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "react-redux";
+import { store } from "@/store";
 
 interface Props extends ChildrenProps {}
 
@@ -16,30 +18,32 @@ const AppProvider = (props: Props) => {
 
   const queryClient = new QueryClient();
   return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>
-        <MUIProvider>
-          <NextTopLoader color={`var(--primary)`} showSpinner={false} />
-          {children}
-          <NetworkStatus />
-        </MUIProvider>
-        <ToastContainer
-          position="top-center"
-          autoClose={3000}
-          hideProgressBar={true}
-          newestOnTop={false}
-          closeOnClick={false}
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          limit={3}
-          pauseOnHover
-          theme="light"
-          transition={Flip}
-          className="md:text-md text-sm"
-        />
-      </QueryClientProvider>
-    </SessionProvider>
+    <Provider store={store}>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          <MUIProvider>
+            <NextTopLoader color={`var(--primary)`} showSpinner={false} />
+            {children}
+            <NetworkStatus />
+          </MUIProvider>
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            hideProgressBar={true}
+            newestOnTop={false}
+            closeOnClick={false}
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            limit={3}
+            pauseOnHover
+            theme="light"
+            transition={Flip}
+            className="md:text-md text-sm"
+          />
+        </QueryClientProvider>
+      </SessionProvider>
+    </Provider>
   );
 };
 
